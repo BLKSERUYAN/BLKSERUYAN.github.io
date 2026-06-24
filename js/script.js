c248a2fc1d96dc63df3cb49b2d946d8b7a93e1d9
BLK SERUYAN <fatih5052012@gmail.com>
2026-06-24 20:29:02 +0700

    hanyar

--- js/script.js
+++ js/script.js
@@ -166,9 +166,32 @@ dropdownToggles.forEach(toggle => {
     }
 
 
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    
+    document.querySelectorAll(".clickable-image").forEach(img => {
+        img.addEventListener("click", () => {
+            modal.style.display = "block";
+            modalImg.src = img.src;
+        });
+    });
+    
+    document.querySelector(".close").onclick = () => {
+        modal.style.display = "none";
+    };
+    
+    modal.onclick = (e) => {
+        if(e.target === modal){
+            modal.style.display = "none";
+        }
+    };
 
 
 });
+
+
+
+
 
 
 
